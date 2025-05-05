57128fec3545daf221bff6b95f0ab54d6fd08993
sawataDTS <sawata.gore@dtskill.com>
2025-05-05 09:49:51 +0530

    learning nest js

---
+++ src/app.controller.ts
@@ -0,0 +1,32 @@
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller('/users')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  // params
+  @Get(':id')
+  getParam(@Param() param: { id: string }) {
+    console.log(param.id, 'Param ID');
+    return 'Success';
+  }
+
+  // query
+  @Get('/')
+  getQuery(@Query() query: any) {
+    console.log(query, 'Query');
+    return 'Success';
+  }
+
+  // parsing body
+  @Post('/')
+  creteUser(@Body() { id, name }: { id: number; name: string }) {
+    console.log(id, 'id');
+    console.log(name, 'name');
+    return {
+      id,
+      name,
+    };
+  }
+}
